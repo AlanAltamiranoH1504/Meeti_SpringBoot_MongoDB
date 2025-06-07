1d458308a3b6e0e1979245587aace17d0f6ca6b3
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-06-06 18:41:33 -0600

    initial commit

---
+++ src/main/java/altamirano/hernandez/meeti_springboot_mongodb/MeetiSpringBootMongoDbApplication.java
@@ -0,0 +1,13 @@
+package altamirano.hernandez.meeti_springboot_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetiSpringBootMongoDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetiSpringBootMongoDbApplication.class, args);
+    }
+
+}
